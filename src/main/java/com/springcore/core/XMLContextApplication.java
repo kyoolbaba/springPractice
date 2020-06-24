2f1a348901e26847b8f5699b49621d2e49c77189
kyoolbaba <kyoolbaba@gmail.com>
2020-06-24 23:32:31 +0530

    Added componentScan in applicationcontext.xml

---
+++ src/main/java/com/springcore/core/XMLContextApplication.java
@@ -0,0 +1,19 @@
+package com.springcore.core;
+
+import com.springcore.core.xmluser.XmlUserDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class XMLContextApplication {
+public static Logger LOGGER= LoggerFactory.getLogger(XMLContextApplication.class);
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext applicationContext= new
+                ClassPathXmlApplicationContext("applicationContext.xml");
+        XmlUserDAO userDAO=applicationContext.getBean(XmlUserDAO.class);
+        LOGGER.info("{} {}",userDAO,userDAO.getXmljdbcConnection());
+        LOGGER.info("Beans Loaded ->{}",(Object)applicationContext.getBeanDefinitionNames());
+
+    }
+
+}
