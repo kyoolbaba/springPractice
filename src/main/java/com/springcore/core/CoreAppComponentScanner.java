f451f23e0963693aa941ccf32021b2bdeb745c09
kyoolbaba <kyoolbaba@gmail.com>
2020-06-24 19:35:49 +0530

    Added another unreachable package to use ComponentScan Annotation

--- src/main/java/com/springcore/core/CoreAppComponentScanner.java
+++ src/main/java/com/springcore/core/CoreAppComponentScanner.java
@@ -1,28 +1,24 @@
 package com.springcore.core;
 
+import com.springcore.components.ComponentDAO;
 import com.springcore.core.compute.CalculateNumbersImpl;
-import com.springcore.core.user.UserDAO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class CoreApplication {
+@ComponentScan("com.springcore.components")
+public class CoreAppComponentScanner {
 
     private static Logger LOGGER= LoggerFactory.getLogger(CoreApplication.class);
 
     public static void main(String[] args) {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(CoreApplication.class, args);
-        CalculateNumbersImpl calculateNumbers=applicationContext.getBean(CalculateNumbersImpl.class);
-        UserDAO userDAO= applicationContext.getBean(UserDAO.class);
-        UserDAO userDAO1=applicationContext.getBean(UserDAO.class);
-        LOGGER.info("{}",userDAO);
-        LOGGER.info("{}",userDAO.getJdbcConnection());
-        LOGGER.info("{}",userDAO.getJdbcConnection());
-        LOGGER.info("{}",userDAO1);
-        LOGGER.info("{}",userDAO1.getJdbcConnection());
+        ComponentDAO componenetDao= applicationContext.getBean(ComponentDAO.class);
+        LOGGER.info("{}",componenetDao);
 
     }
 
