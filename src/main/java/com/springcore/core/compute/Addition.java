1b5a9deb1e55eb1deeac530b87e759c62ebbecd6
kyoolbaba <kyoolbaba@gmail.com>
2020-06-24 17:24:31 +0530

    Refactored code To test @Primary annotation  has higher priority than name of the Component in autowiring

--- src/main/java/com/springcore/core/compute/Addition.java
+++ src/main/java/com/springcore/core/compute/Addition.java
@@ -4,9 +4,9 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class Addition implements Compute {
     public int compute(int firstNumber, int secondNumber) {
-
         return firstNumber+secondNumber;
     }
 }
