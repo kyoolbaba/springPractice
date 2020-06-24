73e23225ffd55c2ee33573f67aa96a8d95c88fe3
kyoolbaba <kyoolbaba@gmail.com>
2020-06-24 16:30:59 +0530

    Refactored Autowired using constructor injection and setter injection and without them

--- src/main/java/com/springcore/core/CalculateNumbersImpl.java
+++ src/main/java/com/springcore/core/CalculateNumbersImpl.java
@@ -8,11 +8,19 @@ import org.springframework.stereotype.Component;
 public class CalculateNumbersImpl {
     @Autowired
     private Compute computeNumber;
-    @Autowired
-    public CalculateNumbersImpl(Compute computeNumber) {
-        this.computeNumber = computeNumber;
-    }
 
+// Type 1 Constructor Injection
+//    public CalculateNumbersImpl(Compute computeNumber) {
+//        this.computeNumber = computeNumber;
+//    }
+
+// Type 2 Setter Injection
+
+//    public void setComputeNumber(Compute computeNumber) {
+//        this.computeNumber = computeNumber;
+//    }
+
+    // Type 3 Even though Constructor and setter are not used then also it is autowired
     public int computeNumbers(int firstNumber , int secondNumber){
 //        need to add or multiply or subtract or divide
         int result=computeNumber.compute(firstNumber,secondNumber);
--- src/main/java/com/springcore/core/compute/Addition.java
+++ src/main/java/com/springcore/core/compute/Addition.java
@@ -1,8 +1,10 @@
 package com.springcore.core.compute;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class Addition implements Compute {
     public int compute(int firstNumber, int secondNumber) {
 
